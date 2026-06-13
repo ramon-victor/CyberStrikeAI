fde4ae24b2c3f80d40868cbee575d476a2248629
ramon-victor <ramonvc19@gmail.com>
2026-06-13 15:53:51 -0300

    fix(docker): downgrade gobuster and amass for go 1.24 compatibility
    Gobuster v3.8.2 and Amass main branch both bumped their go version requirements beyond go 1.24, which broke the tools-builder stage.

--- scripts/docker/install-go-tools.sh
+++ scripts/docker/install-go-tools.sh
@@ -12,9 +12,9 @@ go install github.com/projectdiscovery/httpx/cmd/httpx@v1.6.8
 go install github.com/projectdiscovery/nuclei/v3/cmd/nuclei@v3.3.0
 go install github.com/projectdiscovery/subfinder/v2/cmd/subfinder@v2.6.6
 go install github.com/ffuf/ffuf/v2@v2.1.0
-go install github.com/OJ/gobuster/v3@v3.8.2
+go install github.com/OJ/gobuster/v3@v3.8.1
 go install github.com/hahwul/dalfox/v2@v2.12.0
 go install github.com/projectdiscovery/katana/cmd/katana@v1.1.0
-CGO_ENABLED=0 go install -v github.com/owasp-amass/amass/v5/cmd/amass@main
+CGO_ENABLED=0 go install -v github.com/owasp-amass/amass/v5/cmd/amass@v5.0.1
 go install github.com/tomnomnom/waybackurls@latest
 go install github.com/lc/gau/v2/cmd/gau@latest
